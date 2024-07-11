3754a7f3e7d7bd07e173ecb438e1d8d6a0488703
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 12:58:29 -0300

    Print or return in Functions

---
+++ Class5.py
@@ -0,0 +1,16 @@
+#Print or return in Functions 
+
+#Realizam uma tarefa
+#Calcula e retorna um valor 
+
+def cliente1(nome):
+    print(f'Olá {nome}')
+
+def cliente2(nome):
+   return f'Olá {nome}'
+
+x = cliente1('Maria')
+y = cliente2('Jose')
+
+print(x)
+#print(y)
