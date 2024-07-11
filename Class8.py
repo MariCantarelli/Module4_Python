8b27881c0d143989166e38557a5d9e329bb76d04
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 15:19:53 -0300

    Importing a module

---
+++ Class8.py
@@ -0,0 +1,13 @@
+#Importing a module
+import math
+
+print(math.factorial(40))
+'''fatorial = 4 * 3 * 2 * 1 
+print(fatorial)'''
+
+print(math.ceil(2.7))
+
+print(math.floor(2.7))
+
+'''fatorial = 4 * 3 * 2 * 1 
+print(fatorial)'''
