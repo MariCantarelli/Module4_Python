7160d2a52b8b4097558d29313bf4d270a957d015
Marina Cantarelli <macbarroca@gmail.com>
2024-07-10 22:02:54 -0300

    How functions works

---
+++ Class1.py
@@ -0,0 +1,18 @@
+#How functions works 
+
+#Don't repeat yourself 
+
+def boas_vindas():
+    print("Olá Marcos!")
+    print("Temos 5 laptops em estoque")
+
+boas_vindas()
+
+print("Olá nome")
+print("Olá nome")
+print("Olá nome")
+print("Olá nome")
+print("Olá nome")
+print("Olá nome")
+
+boas_vindas()
