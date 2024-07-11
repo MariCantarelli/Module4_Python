de86ea11dcb1d53cde1de807360215eae7622938
Marina Cantarelli <macbarroca@gmail.com>
2024-07-10 22:17:49 -0300

    Parameters and arguments in a function

---
+++ Class3.py
@@ -0,0 +1,27 @@
+#Parameters and arguments in a function
+
+#Parametro --> Argumento 
+
+def boas_vindas(nome, quantidade):
+    print(f'Olá {nome}.')
+    print(f'Temos {str(quantidade)} laptops em estoque. ')
+
+boas_vindas("Marcos", 5)
+boas_vindas("Ronaldo", 4)
+boas_vindas("Linda", 2)
+
+'''def boas_vindas_Marcos():
+    print("Olá Marcos!")
+    print("Temos 5 laptops em estoque")
+
+def boas_vindas_Ronaldo():
+    print("Olá Ronaldo!")
+    print("Temos 4 laptops em estoque")
+
+def boas_vindas_Linda():
+    print("Olá Linda!")
+    print("Temos 2 laptops em estoque")
+
+boas_vindas_Marcos()
+boas_vindas_Ronaldo()
+boas_vindas_Linda()'''
