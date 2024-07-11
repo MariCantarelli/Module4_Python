46eab3c52c5679e052317f2d79d3973cb2e8b868
Marina Cantarelli <macbarroca@gmail.com>
2024-07-10 22:11:04 -0300

    Creating a sum function

---
+++ Class2.py
@@ -0,0 +1,17 @@
+#Creating a sum function
+
+def add_two_numbers():
+    number1 = 10
+    number2 = 5 
+    result = number1 + number2
+    print (result)
+
+def add_two_numbers1():
+    number1 = 10
+    number2 = 2 
+    result = number1 + number2
+    print (result)
+
+add_two_numbers()
+add_two_numbers1()
+
